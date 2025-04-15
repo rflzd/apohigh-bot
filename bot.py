10b4e26f683859cf8a59bfc69774029959f41137
rflzd <rflzd@yandex.ru>
2025-04-16 01:27:38 +0400

    Fixed import for prematch_match_list_handler to match_list_handler

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ from handlers.subscribe_info_handler import subscribe_info_handler
 from handlers.favorites_handler import favorites_handler
 from handlers.bet_analysis_handler import bet_analysis_handler
 from handlers.match_list_handler import match_list_handler
-from handlers.prematch_match_list_handler import prematch_match_list_handler
 from handlers.match_detail_handler import match_detail_handler
 from handlers.start_handler import start_command  # start_command funksiyasını import edirik
 
